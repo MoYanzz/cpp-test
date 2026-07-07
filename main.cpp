65ef39c11ef392439b7122dda521e0e4f77ac5a1
fairyan <3180520368@qq.com>
2026-07-07 19:11:31 +0800

    2 times

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	cout << "Hello world" << endl;
-	cout << "updated" ; 
-	cout << "more output"; 
+	cout << "updated\n" ; 
+	cout << "more output update"; 
 	return 0;
 } 
